8d5cec78fc2c1f2b064283815adf3d31a055bab9
Jalissa Trinidad <you@example.com>
2017-01-22 16:06:09 -0400

    notification component

--- app/dash/components/Notification.js
+++ app/dash/components/Notification.js
@@ -24,7 +24,7 @@ export class Notification extends React.Component{
 
     render(){
         return(
-            <div ref="notification" className={'callout notification callout'+this.props.type} >
+            <div ref="notification" className={'callout notification callout-'+this.props.type} >
                 <h4>{this.props.title}</h4>
                 <p>{this.props.message}</p>
             </div>
